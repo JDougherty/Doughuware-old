22bea8d5b64c910f082eebe23f5be3e45fc09500
Joseph Dougherty <Joseph@JWDougherty.com>
2013-10-05 16:34:20 -0400

    Added bootstrap. Added thumbnail generation for Documents. Continued template work.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.conf import settings
+import re
+import subprocess
+import os
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -22,3 +25,19 @@ class Document(models.Model):
     file = models.FileField(upload_to="documents/%Y/%m/%d")
     thumbnail = models.ImageField(upload_to="documents/thumbnails/%Y/%m/%d")
     preview = models.ImageField(upload_to="documents/previews/%Y/%m/%d")
+
+    def save_processing(self):
+        if not self.file:
+            return
+
+       
+        thumbnail_path =  re.sub(r'documents/','documents/thumbnail/', os.path.split(self.file.name)[0])+"/"
+        if not os.path.exists(thumbnail_path):
+            os.makedirs(thumbnail_path)
+        thumbnail_path += os.path.splitext(os.path.split(self.file.name)[-1])[0]+".png"
+        params = ['convert', settings.MEDIA_ROOT+self.file.name+"[0]", '-resize','200', settings.MEDIA_ROOT+thumbnail_path]
+        subprocess.check_call(params) 
+        self.thumbnail = settings.MEDIA_URL+thumbnail_path
+        self.save()
+        
+
